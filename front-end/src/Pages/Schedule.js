a8ba6a09e2d204779d8ed3fe5469460dd250eb7c
DESKTOP-A22BN7T\ayonr <ayonrakib@gmail.com>
2022-09-17 17:33:09 -0500

    schedule fixed

--- front-end/src/Pages/Schedule.js
+++ front-end/src/Pages/Schedule.js
@@ -14,23 +14,60 @@ export default function Schedule() {
       </thead>
       <tbody>
         <tr>
-          <td>League Day 1</td>
+          <td>Draft</td>
+          <td>09/24/2022</td>
+          <td>9:00 am</td>
+          <td>12:00 pm</td>
+          <td>Cedar Park Recreation Center</td>
+        </tr>
+        <tr>
+          <td>League Games Day 1 (Teams 1 to 8)</td>
           <td>10/29/2022</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-          <td>@mdo</td>
+          <td>8:30 am</td>
+          <td>12 pm</td>
+          <td>Austin Sports Center (ASC) Woodward St</td>
         </tr>
         <tr>
-          <td>2</td>
-          <td>Jacob</td>
-          <td>Thornton</td>
-          <td>@fat</td>
-          <td>@mdo</td>
+        <td>League Games Day 2 (Teams 9 to 16)</td>
+          <td>10/30/2022</td>
+          <td>8:30 am</td>
+          <td>12 pm</td>
+          <td>Round Rock Sports Center</td>
         </tr>
         <tr>
-          <td>3</td>
-          <td colSpan={2}>Larry the Bird</td>
-          <td>@twitter</td>
+        <td>Quarter Finals</td>
+          <td>11/05/2022</td>
+          <td>3 pm</td>
+          <td>6 pm</td>
+          <td>Round Rock Sports Center</td>
+        </tr>
+        <tr>
+        <td>Semi Finals</td>
+          <td>11/06/2022</td>
+          <td>8:30 am</td>
+          <td>12 pm</td>
+          <td>Round Rock Sports Center</td>
+        </tr>
+        <tr>
+        <td>3rd and 4th place match</td>
+          <td>11/13/2022</td>
+          <td>2 pm</td>
+          <td>3 pm</td>
+          <td>Round Rock Sports Center</td>
+        </tr>
+        <tr>
+        <td>Finals</td>
+          <td>11/13/2022</td>
+          <td>3 pm</td>
+          <td>6 pm</td>
+          <td>Round Rock Sports Center</td>
+        </tr>
+        <tr>
+        <td>Prize Distribution and Dinner</td>
+          <td>11/13/2022</td>
+          <td>6:30 pm</td>
+          <td>10 pm</td>
+          <td>Round Rock Sports Center Pavillion</td>
         </tr>
       </tbody>
     </Table>
