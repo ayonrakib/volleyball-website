8aab26b01dea7931babca446658c91b164b75b9e
DESKTOP-A22BN7T\ayonr <ayonrakib@gmail.com>
2022-09-18 01:13:28 -0500

    profiles added

--- front-end/src/Pages/Profiles.js
+++ front-end/src/Pages/Profiles.js
@@ -1,26 +1,85 @@
 import Card from 'react-bootstrap/Card';
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
 
 export default function Profiles() {
+  let playersCardBlock = [];
+  let playersImagesToNames = {
+    "0" : "Rakib Hasan Ayon",
+    "1" : "Rakib Hasan Ayon",
+    "2" : "Rakib Hasan Ayon", 
+    "3" : "Rakib Hasan Ayon",
+    "4" : "Rakib Hasan Ayon",
+    "5" : "Rashed Jewel",
+  }
+  for(var index = 0; index < 6; index++){
+    console.log(`index is ${index}`)
+    playersCardBlock.push(
+                            <div style={{"justifyContent":"space-between", "marginBottom":"30px"}}>
+                              <Card style={{ width: '18rem' }} key={Math.random()}>
+                                <Card.Img variant="top" src= {process.env.PUBLIC_URL+ `/images/${index}.jpg` } style={{"height":"250px"}}/>
+                                <Card.Body>
+                                  <Card.Title>{playersImagesToNames[index]}</Card.Title>
+                                  <Card.Text>
+                                  {playersImagesToNames[index]} is a setter
+                                  </Card.Text>
+                                </Card.Body>
+                              </Card>
+                            </div>
+                          )  
+                            
+                       
+  }
   return (
-    <Row xs={1} md={2} className="g-4">
-      {Array.from({ length: 4 }).map((_, idx) => (
-        <Col>
-          <Card>
-            <div style={{"display": "flex", "justifyContent":"center"}}>
-                <Card.Img style={{"height":"400px", "width": "500px"}} variant="top" src={process.env.PUBLIC_URL+"./images/ayon.jpg"} />
-            </div>
-            
-            <Card.Body>
-              <Card.Title>Rakib Hasan Ayon</Card.Title>
-              <Card.Text>
-                Rakib plays setter.
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-      ))}
-    </Row>
+    // <div style={{"display":"flex"}}>
+    //   <div>
+    //         <Card style={{ width: '18rem' }}>
+    //           <Card.Img variant="top" src= {process.env.PUBLIC_URL+ `/images/0.jpg` } style={{"height":"250px"}}/>
+    //           <Card.Body>
+    //             <Card.Title>Card Title</Card.Title>
+    //             <Card.Text>
+    //               Some quick example text to build on the card title and make up the
+    //               bulk of the card's content.
+    //             </Card.Text>
+    //           </Card.Body>
+    //         </Card>
+    //   </div>
+
+    //   <div >
+    //         <Card style={{ width: '18rem' }}>
+    //           <Card.Img variant="top" src= {process.env.PUBLIC_URL+ `/images/1.jpg` } style={{"height":"250px"}}/>
+    //           <Card.Body>
+    //             <Card.Title>Card Title</Card.Title>
+    //             <Card.Text>
+    //               Some quick example text to build on the card title and make up the
+    //               bulk of the card's content.
+    //             </Card.Text>
+    //           </Card.Body>
+    //         </Card>
+    //     </div>
+    // </div>
+//     [ <Card style={{ width: '18rem' }} key={Math.random()}>
+//     <Card.Img variant="top" src= {process.env.PUBLIC_URL+ `/images/0.jpg` } style={{"height":"250px"}}/>
+//     <Card.Body>
+//       <Card.Title>Rakib Hasan Ayon</Card.Title>
+//       <Card.Text>
+//         Rakib is a setter
+//       </Card.Text>
+//     </Card.Body>
+//   </Card>,
+//    <Card style={{ width: '18rem' }} key={Math.random()}>
+//    <Card.Img variant="top" src= {process.env.PUBLIC_URL+ `/images/1.jpg` } style={{"height":"250px"}}/>
+//    <Card.Body>
+//      <Card.Title>Rakib Hasan Ayon</Card.Title>
+//      <Card.Text>
+//        Rakib is a setter
+//      </Card.Text>
+//    </Card.Body>
+//  </Card>
+//     ]
+<div style={{"display":"flex", "flexGrow":"1", "justifyContent": "space-around", "flexFlow":"wrap"}}>
+{playersCardBlock}
+</div>
+
+
+
   );
 }
