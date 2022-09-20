9a00e355d30fac9f7acd8a7d3c1be6d635ad38c9
DESKTOP-A22BN7T\ayonr <ayonrakib@gmail.com>
2022-09-19 22:09:05 -0500

    sponsors sample

--- front-end/src/Pages/WelcomePage.js
+++ front-end/src/Pages/WelcomePage.js
@@ -29,20 +29,20 @@ export default function WelcomePage() {
             {/* <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p> */}
           </Carousel.Caption>
         </Carousel.Item>
-        {/* <Carousel.Item>
+        <Carousel.Item>
           <img
             className="d-block w-100"
-            src="holder.js/800x400?text=Third slide&bg=20232a"
+            src={process.env.PUBLIC_URL + "/images/sponsors-2022/selina.jpg"}
             alt="Third slide"
           />
 
           <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>
+            {/* <h3>Third slide label</h3> */}
+            {/* <p>
               Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
+            </p> */}
           </Carousel.Caption>
-        </Carousel.Item> */}
+        </Carousel.Item>
       </Carousel>
     </div>
     
