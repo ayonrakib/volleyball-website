a77ff5556a5a654e14501be44447c2e6e01b2da3
DESKTOP-A22BN7T\ayonr <ayonrakib@gmail.com>
2022-09-19 23:51:04 -0500

    sponsors updated

--- front-end/src/Pages/WelcomePage.js
+++ front-end/src/Pages/WelcomePage.js
@@ -2,46 +2,86 @@ import Carousel from 'react-bootstrap/Carousel';
 
 export default function WelcomePage() {
   return (
-    <div style={{"display":"flex", "justifyContent":"center", "alignContent":"center", "maxWidth":"50%", "maxHeight":"60%"}}>
+    <div style={{"display":"flex", "flexDirection":"column", "justifyContent":"center", "alignContent":"center", "maxWidth":"50%", "maxHeight":"60%"}}>
       <Carousel>
         <Carousel.Item>
             <img
               className="d-block w-100"
               src={process.env.PUBLIC_URL + "/images/sponsors-2022/tavares.png"}
-              
-              alt="First slide"
+              alt="Tavares Pharmacy"
             />
-
-          <Carousel.Caption>
-            {/* <h3 style={{"color":"black"}}>Tavares Pharmacy</h3> */}
-            {/* <p style={{"color":"black"}}>Our honorable sponsor for 2022 tournament!</p> */}
-          </Carousel.Caption>
         </Carousel.Item>
+
         <Carousel.Item>
           <img
             className="d-block w-100"
             src={process.env.PUBLIC_URL + "/images/sponsors-2022/wasi.jpg"}
-            alt="Second slide"
+            alt="Wasi Ahmed"
           />
-
-          <Carousel.Caption>
-            {/* <h3 style={{"color":"black"}}>Wasi Ahmed</h3> */}
-            {/* <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p> */}
-          </Carousel.Caption>
         </Carousel.Item>
+        
         <Carousel.Item>
           <img
             className="d-block w-100"
             src={process.env.PUBLIC_URL + "/images/sponsors-2022/selina.jpg"}
-            alt="Third slide"
+            alt="Selina Ahmed"
           />
+        </Carousel.Item>
 
-          <Carousel.Caption>
-            {/* <h3>Third slide label</h3> */}
-            {/* <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p> */}
-          </Carousel.Caption>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={process.env.PUBLIC_URL + "/images/sponsors-2022/ruvati.jpg"}
+            alt="Selina Ahmed"
+          />
+        </Carousel.Item>
+
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={process.env.PUBLIC_URL + "/images/sponsors-2022/nizam.jpg"}
+            alt="Nizam uddin"
+          />
+        </Carousel.Item>
+
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={process.env.PUBLIC_URL + "/images/sponsors-2022/ibrahim.jpg"}
+            alt="Ibrahim Immigration"
+          />
+        </Carousel.Item>
+
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={process.env.PUBLIC_URL + "/images/sponsors-2022/hvj.jpg"}
+            alt="HVJ"
+          />
+        </Carousel.Item>
+
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={process.env.PUBLIC_URL + "/images/sponsors-2022/dash.jpg"}
+            alt="Dash Realty"
+          />
+        </Carousel.Item>
+
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={process.env.PUBLIC_URL + "/images/sponsors-2022/zia.jpg"}
+            alt="Zia Syed"
+          />
+        </Carousel.Item>
+
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={process.env.PUBLIC_URL + "/images/sponsors-2022/kochi.jpg"}
+            alt="Afsar Zaman Kochi"
+          />
         </Carousel.Item>
       </Carousel>
     </div>
