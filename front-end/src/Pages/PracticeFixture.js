8cfdd7a1e2d4023533f1c15ce3f7181c6b7acbcf
RAKIBAYON\ayonr <ayonrakib@gmail.com>
2022-10-14 00:03:13 +0600

    practice match updated

--- front-end/src/Pages/PracticeFixture.js
+++ front-end/src/Pages/PracticeFixture.js
@@ -2,7 +2,7 @@ import Table from 'react-bootstrap/Table';
 
 export default function PracticeFixture() {
   return (
-    <div>
+    <div style={{"marginTop":"30px"}}>
         <Table responsive striped bordered hover variant='dark' className='tableStyle'>
             <thead>
                     <tr>
@@ -51,31 +51,41 @@ export default function PracticeFixture() {
                     <td style={{"textAlign":"center"}}>5A, 5B</td>
                     <td style={{"textAlign":"center"}}>Abu Sohel, Rupom, Manar, Moinul, Rashed, Mashu</td>
                 </tr>
-            </tbody>
-        </Table>
-        <Table responsive striped bordered hover variant='dark' className='tableStyle'>
-            <thead>
-                    <tr>
-                        <th 
-                            style={{"backgroundColor":"yellow", "justifyContent":"center","color":"black", "textAlign":"center"}} 
-                            colSpan={4}>Practice Round 2: Round Rock Sports Center,  
-                            <a 
-                                href='https://g.page/rrsportscenter?share'
-                            > 
-                                2400 Chisholm Trail Rd
-                            </a>
-                        </th>
-                    </tr>   
-                    <tr>
-                        <th style={{"backgroundColor":"yellow", "justifyContent":"center","color":"black", "textAlign":"center"}} colSpan={4}>Reporting time: 7:45 pm</th>
-                    </tr>    
-            </thead>
-            <tbody>
                 <tr>
-                    <td style={{"textAlign":"center", "fontWeight":"bold"}}>Date</td>
-                    <td style={{"textAlign":"center", "fontWeight":"bold"}}>Time</td>
-                    <td style={{"textAlign":"center", "fontWeight":"bold"}}>Courts</td>
-                    <td style={{"textAlign":"center", "fontWeight":"bold"}}>Captains</td>
+                    <td style={{"textAlign":"center"}}>10/14/2022</td>
+                    <td style={{"textAlign":"center"}}>8 pm - 10 pm</td>
+                    <td style={{"textAlign":"center"}}>6B</td>
+                    <td style={{"textAlign":"center"}}>Moinul, Zahedul, Shaheen</td>
+                </tr>
+                <tr>
+                    <td style={{"textAlign":"center"}}>10/18/2022</td>
+                    <td style={{"textAlign":"center"}}>8 pm - 10 pm</td>
+                    <td style={{"textAlign":"center"}}>5A, 5B</td>
+                    <td style={{"textAlign":"center"}}>Abu Sohel, Rupom, Shaheen, Rezwan, Mona, Nafis</td>
+                </tr>
+                <tr>
+                    <td style={{"textAlign":"center"}}>10/19/2022</td>
+                    <td style={{"textAlign":"center"}}>8 pm - 10 pm</td>
+                    <td style={{"textAlign":"center"}}>3B</td>
+                    <td style={{"textAlign":"center"}}>Omar, Manar, Moinul</td>
+                </tr>
+                <tr>
+                    <td style={{"textAlign":"center"}}>10/21/2022</td>
+                    <td style={{"textAlign":"center"}}>8 pm - 10 pm</td>
+                    <td style={{"textAlign":"center"}}>1A</td>
+                    <td style={{"textAlign":"center"}}>Munna, Sami, Zahedul</td>
+                </tr>
+                <tr>
+                    <td style={{"textAlign":"center"}}>10/23/2022</td>
+                    <td style={{"textAlign":"center"}}>8 pm - 10 pm</td>
+                    <td style={{"textAlign":"center"}}>2A</td>
+                    <td style={{"textAlign":"center"}}>Mona, Rezwan, Manar</td>
+                </tr>
+                <tr>
+                    <td style={{"textAlign":"center"}}>10/24/2022</td>
+                    <td style={{"textAlign":"center"}}>8 pm - 10 pm</td>
+                    <td style={{"textAlign":"center"}}>1A</td>
+                    <td style={{"textAlign":"center"}}>Shajal, Rashed, Masu</td>
                 </tr>
                 <tr>
                     <td style={{"textAlign":"center"}}>10/24/2022</td>
@@ -100,6 +110,45 @@ export default function PracticeFixture() {
                     <td style={{"textAlign":"center"}}>8 pm - 10 pm</td>
                     <td style={{"textAlign":"center"}}>4A, 4B</td>
                     <td style={{"textAlign":"center"}}>Shajal, Mashu, Omar, Moinul, Shaheen, Mona</td>
+                </tr>
+            </tbody>
+        </Table>
+        
+        <Table responsive striped bordered hover variant='dark' className='tableStyle'>
+            <thead>
+                    <tr>
+                        <th 
+                            style={{"backgroundColor":"yellow", "justifyContent":"center","color":"black", "textAlign":"center"}} 
+                            colSpan={4}>Practice Round 2: Austin Sports Center-Woodward,  
+                            <a 
+                                href='https://goo.gl/maps/o7beunWFW6Bi6wJ46'
+                            > 
+                                425 Woodward St, Austin
+                            </a>
+                        </th>
+                    </tr>   
+                    <tr>
+                        <th style={{"backgroundColor":"yellow", "justifyContent":"center","color":"black", "textAlign":"center"}} colSpan={4}>Reporting time: 15 minutes before match</th>
+                    </tr>    
+            </thead>
+            <tbody>
+                <tr>
+                    <td style={{"textAlign":"center", "fontWeight":"bold"}}>Date</td>
+                    <td style={{"textAlign":"center", "fontWeight":"bold"}}>Time</td>
+                    <td style={{"textAlign":"center", "fontWeight":"bold"}}>Courts</td>
+                    <td style={{"textAlign":"center", "fontWeight":"bold"}}>Captains</td>
+                </tr>
+                <tr>
+                    <td style={{"textAlign":"center"}}>10/19/2022</td>
+                    <td style={{"textAlign":"center"}}>7:30 pm - 9:30 pm</td>
+                    <td style={{"textAlign":"center"}}>CVB1, CVB2</td>
+                    <td style={{"textAlign":"center"}}>Shajal, Sami, Polin, Munna, Rashed</td>
+                </tr>
+                <tr>
+                    <td style={{"textAlign":"center"}}>10/22/2022</td>
+                    <td style={{"textAlign":"center"}}>10 am - 12 pm</td>
+                    <td style={{"textAlign":"center"}}>CVB2</td>
+                    <td style={{"textAlign":"center"}}>Abu Sohel, Rupom, Nafis</td>
                 </tr>
             </tbody>
         </Table>
